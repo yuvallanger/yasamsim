6b8b0c3869256aab19e59db0ec91d2a1d707ca6e
Yuval Langer <yuval.langer@gmail.com>
2015-07-23 17:13:07 +0300

    Converting space-based indentation into tab-based indentation

--- src/Main.hs
+++ src/Main.hs
@@ -2,102 +2,102 @@ module Main where
 
 
 import Data.Monoid
-    ( (<>)
-    , mempty
-    )
+	( (<>)
+	, mempty
+	)
 import Graphics.Gloss.Interface.IO.Game
-    ( playIO
-    , Event
-    , Display (InWindow)
-    , Picture
-    , black
-    )
+	( playIO
+	, Event
+	, Display (InWindow)
+	, Picture
+	, black
+	)
 
 
 data Point
-    = Point
-    { pointX :: !Int
-    , pointY :: !Int
-    , pointZ :: !Int
-    }
+	= Point
+	{ pointX :: !Int
+	, pointY :: !Int
+	, pointZ :: !Int
+	}
 
 
 data Orientation
-    = FacingLeft
-    | FacingRight
-    | FacingAway
-    | FacingPlayer
+	= FacingLeft
+	| FacingRight
+	| FacingAway
+	| FacingPlayer
 
 data Character
-    = Character
-    { characterPosition    :: !Main.Point
-    , characterPower       :: !Int
-    , characterSpeed       :: !Int
-    , characterWeapon      :: !Weapon
-    , characterHealth      :: !Int
-    , characterEnergy      :: !Int
-    , characterState       :: !CharacterState
-    , characterOrientation :: !Orientation
-    }
+	= Character
+	{ characterPosition    :: !Main.Point
+	, characterPower       :: !Int
+	, characterSpeed       :: !Int
+	, characterWeapon      :: !Weapon
+	, characterHealth      :: !Int
+	, characterEnergy      :: !Int
+	, characterState       :: !CharacterState
+	, characterOrientation :: !Orientation
+	}
 
 
 data CharacterState
-    = Walk
-    | Jump
+	= Walk
+	| Jump
 
 
 data Weapon
-    = Fist
-    | Club
+	= Fist
+	| Club
 
 
 data Item
-    = Crate
-    | Medipack
+	= Crate
+	| Medipack
 
 
 data World
-    = World
-    { player    :: CharacterState
-    , civilians :: [CharacterState]
-    , items     :: [Item]
-    }
+	= World
+	{ player    :: CharacterState
+	, civilians :: [CharacterState]
+	, items     :: [Item]
+	}
 
 
 drawScene :: World -> IO Picture
 drawScene w = return (background)
-    where
-    background = mempty
+	where
+	background = mempty
 
 
 handleInput :: Event -> World -> IO World
 handleInput event world = do
-    putStrLn "handleInput"
-    return world
+	putStrLn "handleInput"
+	return world
 
 
 stepGame :: Float -> World -> IO World
 stepGame time world = do
-    putStrLn "stepGame"
-    return world
+	putStrLn "stepGame"
+	return world
 
 
 initialWorld :: World
 initialWorld
-    = World
-    { player    = undefined
-    , civilians = undefined
-    , items     = undefined
-    }
+	= World
+	{ player    = undefined
+	, civilians = undefined
+	, items     = undefined
+	}
 
 
 main :: IO ()
 main = do
-    playIO
-        (InWindow "Yasam Sim" (1, 1) (500, 500))
-        black
-        10
-        initialWorld
-        drawScene
-        handleInput
-        stepGame
+	playIO
+		(InWindow "Yasam Sim" (1, 1) (500, 500))
+		black
+		10
+		initialWorld
+		drawScene
+		handleInput
+		stepGame
